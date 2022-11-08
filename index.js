f79172b64ab06c65a05cd5acec98e9505b6596a7
rahul-sarker18 <rahulsarker1495@gmail.com>
2022-11-08 13:44:48 +0530

    midelwait add

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = express();
 const port = process.env.PORT ||5000;
 
 //midel waer
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
 app.get('/', (req ,res)=>{
     res.send('welcome !!')
 })
