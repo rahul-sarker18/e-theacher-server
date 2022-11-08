ed83481de171b8af95baed9adfaa443b59da387b
rahul-sarker18 <rahulsarker1495@gmail.com>
2022-11-08 14:46:16 +0530

    get services in mongodb

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT ||5000;
 
 //midel waer
 
 app.use(cors());
 app.use(express.json());
+
+
+// mongodb atlast conected 
+
+//username : all-modul
+//password:  YFpNtBkSaE24ELEw
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://all-modul:YFpNtBkSaE24ELEw@cluster0.rd0mbf3.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    try {
+      const createdb = client.db("freeeservices").collection("searvices");
+      const revewdb = client.db("freeeservices").collection("revew");
+  
+  
+      
+      //get all searvices
+      app.get('/searvices' ,async (req , res)=>{
+        const query = {};
+        const curture = createdb.find(query);
+        const result = await curture.toArray();
+        res.send(result)
+      })
+     
+    } finally {
+    }
+  }
+  run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
 
 
 
